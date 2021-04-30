75c5ba34942b4fef41f1fa993e42d40b2f9e33a2
mariaGuadalupeB <mariagballesi@gmail.com>
2021-04-30 20:09:39 -0300

    style home + edit skills

--- skillsearch/api/config/index.js
+++ skillsearch/api/config/index.js
@@ -1,19 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const { DB_NAME, DB_PASSWORD } = process.env;
+const { DB_CONNECTION } = process.env;
 
 //MONGO ATLAS DATA BASE
 mongoose
-  .connect(
-    `mongodb+srv://SkillsSearch:${DB_PASSWORD}@skillssearchdb.soomp.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  )
+  .connect(`${DB_CONNECTION}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
   .then(() => console.log("DB CONNECT!!"))
   .catch((e) => console.log("DB NO CONNECT ERROR", e));
 
--- skillsearch/src/Components/App.jsx
+++ skillsearch/src/Components/App.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { Route, Redirect, Switch } from "react-router-dom";
 import ShowSkills from "./ShowSkills";
 import Home from "./Home";
 import EditSkills from "./EditSkills";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-const App= () => {
-
+const App = () => {
   return (
-    <>
- 
-      <Navbar/>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <Navbar />
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/showSkills' component={ShowSkills}/>
-        <Route path='/editSkills' component={EditSkills}/>
-        <Redirect to='/'/>
-      </Switch> 
-      <Footer /> 
-    </>
+        <Route exact path="/" component={Home} />
+        <Route path="/showSkills" component={ShowSkills} />
+        <Route path="/editSkills" component={EditSkills} />
+        <Redirect to="/" />
+      </Switch>
+      <Footer />
+    </div>
   );
 };
 
